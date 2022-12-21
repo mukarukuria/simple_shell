c561bfa43cafa7a20f80ddbb2d353c046bb341fb
mukarukuria <timothy.kuria@strathmore.edu>
2022-12-21 10:57:48 +0000

    Functions that define and control environment

---
+++ envs.c
@@ -0,0 +1,175 @@
+#include "main.h"
+
+/**
+ * _exit - exits the shell with or without a return of status n
+ * @arv: array of words of the entered line
+ *
+ * Return: void
+ */
+
+void _exit(char **arv)
+{
+	int i, n;
+
+	if (arv[1])
+	{
+		n = _atoi(arv[1]);
+		if (n <= -1)
+			n = 2;
+		freearv(arv);
+		exit(n);
+	}
+	for (i = 0; arv[i]; i++)
+		free(arv[i]);
+	free(arv);
+	exit(0);
+}
+
+/**
+ * _env - prints the current environment
+ * @arv: array of arguments
+ *
+ * Return: void
+ */
+
+void _env(char **arv __attribute__ ((unused)))
+{
+
+	int i;
+
+	for (i = 0; environ[i]; i++)
+	{
+		_puts(environ[i]);
+		_puts("\n");
+	}
+
+}
+
+/**
+ * _setenv - Initialize a new environment variable, or modify an existing one
+ * @arv: array of entered words
+ *
+ * Return: void
+ */
+
+void _setenv(char **arv)
+{
+	int i, j, k;
+
+	if (!arv[1] || !arv[2])
+	{
+		perror(_getenv("_"));
+		return;
+	}
+
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (arv[1][j] == environ[i][j])
+		{
+			while (arv[1][j])
+			{
+				if (arv[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (arv[1][j] == '\0')
+			{
+				k = 0;
+				while (arv[2][k])
+				{
+					environ[i][j + 1 + k] = arv[2][k];
+					k++;
+				}
+				environ[i][j + 1 + k] = '\0';
+				return;
+			}
+		}
+	}
+	if (!environ[i])
+	{
+
+		environ[i] = concat_all(arv[1], "=", arv[2]);
+		environ[i + 1] = '\0';
+
+	}
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @arv: array of entered words
+ *
+ * Return: void
+ */
+void _unsetenv(char **arv)
+{
+	int i, j;
+
+	if (!arv[1])
+	{
+		perror(_getenv("_"));
+		return;
+	}
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (arv[1][j] == environ[i][j])
+		{
+			while (arv[1][j])
+			{
+				if (arv[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (arv[1][j] == '\0')
+			{
+				free(environ[i]);
+				environ[i] = environ[i + 1];
+				while (environ[i])
+				{
+					environ[i] = environ[i + 1];
+					i++;
+				}
+				return;
+			}
+		}
+	}
+}
+
+/**
+ * _getenv - gets the value of the global variable
+ * @name: name of the global variable
+ *
+ * Return: string of value
+ */
+
+char *_getenv(const char *name)
+{
+	int i, j;
+	char *value;
+
+	if (!name)
+		return (NULL);
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (name[j] == environ[i][j])
+		{
+			while (name[j])
+			{
+				if (name[j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (name[j] == '\0')
+			{
+				value = (environ[i] + j + 1);
+				return (value);
+			}
+		}
+	}
+	return (0);
+}
