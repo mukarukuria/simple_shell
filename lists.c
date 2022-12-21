f544f3efafcc53cde04285ccc3a13254b52b6ce6
mukarukuria <timothy.kuria@strathmore.edu>
2022-12-21 11:51:54 +0000

    Struct and lists functions

---
+++ lists.c
@@ -0,0 +1,99 @@
+#include "main.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to pointer to our linked list
+ * @str: pointer to string in previous first node
+ *
+ * Return: address of the new element/node
+ */
+
+list_path *add_node_end(list_path **head, char *str)
+{
+
+	list_path *tmp;
+	list_path *new;
+
+	new = malloc(sizeof(list_path));
+
+	if (!new || !str)
+	{
+		return (NULL);
+	}
+
+	new->dir = str;
+
+	new->p = '\0';
+	if (!*head)
+	{
+		*head = new;
+	}
+	else
+	{
+		tmp = *head;
+
+		while (tmp->p)
+		{
+
+			tmp = tmp->p;
+		}
+
+		tmp->p = new;
+	}
+
+	return (*head);
+}
+
+
+/**
+ * linkpath - creates a linked list for path directories
+ * @path: string of path value
+ *
+ * Return: pointer to the created linked list
+ */
+
+list_path *linkpath(char *path)
+{
+	list_path *head = '\0';
+	char *token;
+	char *cpath = _strdup(path);
+
+	token = strtok(cpath, ":");
+	while (token)
+	{
+		head = add_node_end(&head, token);
+		token = strtok(NULL, ":");
+	}
+
+	return (head);
+}
+
+/**
+ * _which - finds the pathname of a filename
+ * @filename: name of file or command
+ * @head: head of linked list of path directories
+ *
+ * Return: pathname of filename or NULL if no match
+ */
+
+char *_which(char *filename, list_path *head)
+{
+	struct stat st;
+	char *string;
+
+	list_path *tmp = head;
+
+	while (tmp)
+	{
+
+		string = concat_all(tmp->dir, "/", filename);
+		if (stat(string, &st) == 0)
+		{
+			return (string);
+		}
+		free(string);
+		tmp = tmp->p;
+	}
+
+	return (NULL);
+}
