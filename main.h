16a67a8ccbd122281e1b1864e1b26c07c7fa1207
mukarukuria <timothy.kuria@strathmore.edu>
2022-12-21 10:54:29 +0000

    Update on main header

--- main.h
+++ main.h
@@ -12,11 +12,57 @@
 #include <sys/stat.h>
 #include <signal.h>
 
-/* Done Functions */
+/**
+ * struct list_path - Linked list containing PATH directories
+ * @dir: directory in path
+ * @p: pointer to next node
+ */
+
+typedef struct list_path
+{
+	char *dir;
+	struct list_path *p;
+} list_path;
+
+extern char **environ;
+
+
+/**
+ * struct mybuild - pointer to function with corresponding buildin command
+ * @name: buildin command
+ * @func: execute the buildin command
+ */
+typedef struct mybuild
+{
+	char *name;
+	void (*func)(char **);
+} mybuild;
+
+
+/* Functions done in previous exercises */
 
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
 int _atoi(char *s);
+char *_strdup(char *str);
+
+char *concat_all(char *name, char *sep, char *value);
+char **splitstring(char *str, const char *delim);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/** Environment Functions **/
+
+char *_getenv(const char *name);
+void _env(char **arv);
+void _setenv(char **arv);
+void _unsetenv(char **arv);
+void _exit(char **arv);
+
+
+/* Terminal Checkers */
+void sig_handler(int sig_num);
+void _EOF(int len, char *buff);
+void _isatty(void);
 
 #endif
