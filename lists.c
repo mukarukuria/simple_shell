05312a3c6c2c39a3646518e55eb82c3aaab546cf
mukarukuria <timothy.kuria@strathmore.edu>
2022-12-22 06:31:13 +0000

    Update on functions

--- lists.c
+++ lists.c
@@ -1,6 +1,42 @@
 #include "main.h"
 
 /**
+ * _getenv - gets the value of the global variable
+ * @name: name of the global variable
+ *
+ * Return: string of value
+ */
+
+char *_getenv(const char *name)
+{
+	int i, j;
+	char *value;
+
+	if (!name)
+		return (NULL);
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (name[j] == environ[i][j])
+		{
+			while (name[j])
+			{
+				if (name[j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (name[j] == '\0')
+			{
+				value = (environ[i] + j + 1);
+				return (value);
+			}
+		}
+	}
+	return (0);
+}
+
+/**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to pointer to our linked list
  * @str: pointer to string in previous first node
